2e34e3e8f64c061cedb410327b959cb52908ef02
AnnGoro <gorobchenko.anna@gmail.com>
2016-08-02 14:27:34 -0700

    added code for getting access token  - its working

--- AlamofirePodsHome/ViewController.swift
+++ AlamofirePodsHome/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import VK_ios_sdk
-class ViewController : UIViewController {
+class ViewController : UIViewController, UIWebViewDelegate {
     
 @IBOutlet weak var authWebView: UIWebView!
      var webViewController = WebViewController()
@@ -16,32 +16,60 @@ class ViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        authWebView.delegate = self
+        
         let defaults = NSUserDefaults.standardUserDefaults()
-        if let access_token = defaults.stringForKey("access_token") {
-            print(access_token)
-        } else {            
-            if let url = NSURL(string: Authorization.REQUEST_STR) {
-                
-                let request = NSURLRequest(URL: url)
-                authWebView.loadRequest(request)
-                webViewController.webViewDidStartLoad(authWebView)
-                
-            }
+       
+        
+        let myURL = NSURL(string: Authorization.REQUEST_STR);
+        let myURLRequest:NSURLRequest = NSURLRequest(URL: myURL!);
+    
+                print ("load request")
+      
+                authWebView.loadRequest(myURLRequest)
+        
 
             
         }
             
-        
-    }
-    
-
-    
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func webViewDidFinishLoad(authWebView: UIWebView) {
+        
+        print ("webViewDidFinishLoad")
+        
+        //создадим хеш-таблицу для хранения данных
+        var user: [String : String] = [String : String]()
+        //смотрим на адрес открытой станицы
+        let currentURL : String = (authWebView.request?.URL!.absoluteString)!
+        // var currentURL = webView.request!.URL!.absoluteString;
+        var textRange = currentURL.lowercaseString.rangeOfString("access_token".lowercaseString)
+        //смотрим, содержится ли в адресе информация о токене
+        if textRange != nil {
+            //Ура, содержится, вытягиваем ее
+            var data: [String] = currentURL.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: "=&"))
+            user["access_token"] = data[1]
+            user["expires_in"] = data[3]
+            user["user_id"] = data[5]
+        
+            
+            NSUserDefaults.standardUserDefaults().setObject(user["access_token"], forKey: "access_token")
+            print( NSUserDefaults.standardUserDefaults().stringForKey("access_token"))
+            
+            NSUserDefaults.standardUserDefaults().setObject(user["user_id"], forKey: "user_id")
+            
+            print ( NSUserDefaults.standardUserDefaults().stringForKey("user_id"))
+            
+            NSUserDefaults.standardUserDefaults().setObject(user["expires_in"], forKey: "expires_in")
+            
+            print(NSUserDefaults.standardUserDefaults().stringForKey("expires_in"))
+            
+           
+                   }
     }
 
 }
--- AlamofirePodsHome/WebViewController.swift
+++ AlamofirePodsHome/WebViewController.swift
@@ -12,11 +12,12 @@ import UIKit
 class WebViewController:UIWebView, UIWebViewDelegate {
 
     
-    func webViewDidStartLoad(webView: UIWebView) {
+    func webViewDidFinishLoad(webView: UIWebView) {
         //создадим хеш-таблицу для хранения данных
         var user: [String : String] = [String : String]()
         //смотрим на адрес открытой станицы
-        var currentURL: String = webView.request!.URL!.absoluteString
+        let currentURL : String = (webView.request?.URL!.absoluteString)!
+       // var currentURL = webView.request!.URL!.absoluteString;
         var textRange = currentURL.lowercaseString.rangeOfString("access_token".lowercaseString)
         //смотрим, содержится ли в адресе информация о токене
         if textRange != nil {
@@ -25,11 +26,11 @@ class WebViewController:UIWebView, UIWebViewDelegate {
             user["access_token"] = data[1]
             user["expires_in"] = data[3]
             user["user_id"] = data[5]
-            let defaults = NSUserDefaults.standardUserDefaults()
-            defaults.setObject(user["access_token"], forKey: "access_token")
-            defaults.setObject(user["user_id"], forKey: "user_id")
-            defaults.setObject(user["expires_in"], forKey: "expires_in")
-            
+           
+            NSUserDefaults.standardUserDefaults().setObject(user["access_token"], forKey: "access_token")
+            NSUserDefaults.standardUserDefaults().setObject(user["user_id"], forKey: "user_id")
+            NSUserDefaults.standardUserDefaults().setObject(user["expires_in"], forKey: "expires_in")
+            NSUserDefaults.standardUserDefaults().synchronize()
             //NSUserDefaults.setValue(user["access_token"], forKey: "access_token")
             //NSUserDefaults.setValue(user["user_id"], forKey: "user_id")
             //NSUserDefaults.setValue(user["expires_in"], forKey: "expires_in")
