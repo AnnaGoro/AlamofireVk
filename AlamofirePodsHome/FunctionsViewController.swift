e80ac81a4f38bdef569a1cc4d541f95deea4b2dd
AnnGoro <gorobchenko.anna@gmail.com>
2016-08-03 07:31:52 -0700

    added reload data, tableView loads data from alamofire request

--- AlamofirePodsHome/FunctionsViewController.swift
+++ AlamofirePodsHome/FunctionsViewController.swift
@@ -10,27 +10,40 @@ import Foundation
 import UIKit
 
 class FunctionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
     
-    let array = ["1", "02", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
-    
+    @IBOutlet weak var tableData: UITableView!
+    //let array = ["1", "02", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+    var array: [String] = [String] ()
     var audioGetService = AudioGetService()
     var userGetService = UserGetService()
     
+    var userModelArray : [UserModel] = [UserModel] ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var owner_id = "owner_id=" + NSUserDefaults.standardUserDefaults().stringForKey("user_id")!
-        print("getAudioById1 (owner_id)")
-        audioGetService.getAudioById1 (owner_id)
-         print("getPopularAudioCount(only_eng=1, count: count=10)")
-        audioGetService.getPopularAudioCount("only_eng=1", count: "count=10")
+        // var owner_id = "owner_id=" + NSUserDefaults.standardUserDefaults().stringForKey("user_id")!
+        //print("getAudioById1 (owner_id)")
+        // audioGetService.getAudioById1 (owner_id)
+        // print("getPopularAudioCount(only_eng=1, count: count=10)")
+        //audioGetService.getPopularAudioCount("only_eng=1", count: "count=10")
         
         userGetService.getUserById1() { (data) in
-            print ("get user by id\(data.1.toJSONString())   \(data.0)")
-        
-        
+            var userModelArray = data.1
+            for value in userModelArray {
+            
+            self.array.append(value.first_name!)
+            
+            }
+            
+            self.tableData.reloadData()
+            
+            print ("get user by id \(data.1[0].toJSONString())   \(data.0)")
+            print ("get user from array \(userModelArray[0].first_name)   \(data.0)")
+         
         }
         
-        
+       
     }
     
     
